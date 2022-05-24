483a88545964ac990b734b9856deaf6c14ba5f3c
Harin <57610449+gharynsl123@users.noreply.github.com>
2022-05-24 13:36:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kisahnabi/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kisahnabi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kisahnabi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
